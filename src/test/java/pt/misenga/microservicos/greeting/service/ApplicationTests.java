09a5a2121da567f43f69312ec6531a2d190a6888
Bruno Freire <brunofreire14@gmail.com>
2022-12-29 23:01:07 +0000

    - implementação de spring actuator

--- src/test/java/pt/misenga/microservicos/greeting/service/ApplicationTests.java
+++ src/test/java/pt/misenga/microservicos/greeting/service/ApplicationTests.java
@@ -1,12 +1,11 @@
 package pt.misenga.microservicos.greeting.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
